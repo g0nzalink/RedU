c472d4c84e0c9e5f02cf49dfd865cd984682c323
EmPuiquin24 <hepuiquin@proton.me>
2025-05-14 12:01:26 -0500

    Añadir base del proyecto

---
+++ src/main/java/com/example/backendredu/BackendReduApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendredu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendReduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendReduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendredu/BackendReduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendredu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendReduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
