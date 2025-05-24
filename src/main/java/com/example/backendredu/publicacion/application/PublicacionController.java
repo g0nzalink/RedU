88bdf6207b7462f2c46c371b4271dbf90f4dc03e
Gonzalink <gonzalink1907@gmail.com>
2025-05-24 17:03:52 -0500

    endpoints de publicacion terminados

--- src/main/java/com/example/backendredu/publicacion/application/PublicacionController.java
+++ src/main/java/com/example/backendredu/publicacion/application/PublicacionController.java
@@ -1,5 +1,33 @@
 package com.example.backendredu.publicacion.application;
 
+import com.example.backendredu.publicacion.domain.Publicacion;
+import com.example.backendredu.publicacion.domain.PublicacionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/publicacion")
+@RequiredArgsConstructor
 public class PublicacionController {
+
+    private final PublicacionService publicacionService;
+
+    //private final ModelMapper modelMapper;
+
+    @GetMapping("/{idPublicacion}")
+    public ResponseEntity<Publicacion> getPublicacion(@PathVariable("idPublicacion") Long idPub){
+        return ResponseEntity.ok(publicacionService.getPublicacionById(idPub));
+    }
+
+    @PostMapping("/publicacion")
+    public ResponseEntity<Publicacion> createPublicacion(@RequestBody Publicacion publicacion){
+        Publicacion createdPublicacion = publicacionService.createPublicacion(publicacion);
+        return ResponseEntity.created(URI.create("http://localhost/publicacion/" + createdPublicacion.getId())).body(createdPublicacion);
+    }
+
+
 }
 
